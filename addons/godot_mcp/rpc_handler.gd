8f183a3c8afce61b021087002898c35da13271ef
miniruiii <12454298@qq.com>
2026-05-21 23:39:11 +0800

    feat(godot): add game.* and input.* RPC routes to rpc_handler.gd
    - Added RuntimeCommandsClass and InputCommandsClass preloads
    - Created runtime_commands and input_commands instances in _init()
    - Added 19 game.* routes pointing to runtime_commands
    - Added 7 input.* routes pointing to input_commands
    - Extended node.* routes with 5 new node tools from scene_editor.gd
    
    Rui <veryrui@gmail.com>

--- addons/godot_mcp/rpc_handler.gd
+++ addons/godot_mcp/rpc_handler.gd
@@ -3,16 +3,22 @@ extends RefCounted
 const SceneEditorClass = preload("res://addons/godot_mcp/editors/scene_editor.gd")
 const ScriptEditorClass = preload("res://addons/godot_mcp/editors/script_editor.gd")
 const ProjectEditorClass = preload("res://addons/godot_mcp/editors/project_editor.gd")
+const RuntimeCommandsClass = preload("res://addons/godot_mcp/editors/runtime_commands.gd")
+const InputCommandsClass = preload("res://addons/godot_mcp/editors/input_commands.gd")
 const Utils = preload("res://addons/godot_mcp/utils.gd")
 
 var scene_editor_inst: SceneEditorClass
 var script_editor_inst: ScriptEditorClass
 var project_editor_inst: ProjectEditorClass
+var runtime_commands: RuntimeCommandsClass
+var input_commands: InputCommandsClass
 
 func _init():
     scene_editor_inst = SceneEditorClass.new()
     script_editor_inst = ScriptEditorClass.new()
     project_editor_inst = ProjectEditorClass.new()
+    runtime_commands = RuntimeCommandsClass.new()
+    input_commands = InputCommandsClass.new()
 
 func handle(message: String) -> String:
     var parsed = JSON.parse_string(message)
@@ -68,6 +74,71 @@ func _route(method: String, params: Dictionary) -> Dictionary:
             return { "result": Utils.get_engine_info() }
         "handshake":
             return { "result": { "version": Utils.PLUGIN_VERSION, "godot_version": Engine.get_version_info()["string"] } }
+        # game.* routes for runtime commands (19 tools)
+        "game.get_tree":
+            return runtime_commands.get_tree(params)
+        "game.get_node_properties":
+            return runtime_commands.get_node_properties(params)
+        "game.set_node_property":
+            return runtime_commands.set_node_property(params)
+        "game.execute_script":
+            return runtime_commands.execute_script(params)
+        "game.find_nodes_by_script":
+            return runtime_commands.find_nodes_by_script(params)
+        "game.get_autoload":
+            return runtime_commands.get_autoload(params)
+        "game.batch_get_properties":
+            return runtime_commands.batch_get_properties(params)
+        "game.find_ui_elements":
+            return runtime_commands.find_ui_elements(params)
+        "game.click_button_by_text":
+            return runtime_commands.click_button_by_text(params)
+        "game.wait_for_node":
+            return runtime_commands.wait_for_node(params)
+        "game.find_nearby_nodes":
+            return runtime_commands.find_nearby_nodes(params)
+        "game.navigate_to":
+            return runtime_commands.navigate_to(params)
+        "game.move_to":
+            return runtime_commands.move_to(params)
+        "game.get_game_node_property":
+            return runtime_commands.get_game_node_property(params)
+        "game.capture_frames":
+            return runtime_commands.capture_frames(params)
+        "game.monitor_properties":
+            return runtime_commands.monitor_properties(params)
+        "game.start_recording":
+            return runtime_commands.start_recording(params)
+        "game.stop_recording":
+            return runtime_commands.stop_recording(params)
+        "game.replay_recording":
+            return runtime_commands.replay_recording(params)
+        # input.* routes for input commands (7 tools)
+        "input.simulate_key":
+            return input_commands.simulate_key(params)
+        "input.simulate_mouse_click":
+            return input_commands.simulate_mouse_click(params)
+        "input.simulate_mouse_move":
+            return input_commands.simulate_mouse_move(params)
+        "input.simulate_action":
+            return input_commands.simulate_action(params)
+        "input.simulate_sequence":
+            return input_commands.simulate_sequence(params)
+        "input.get_input_actions":
+            return input_commands.get_input_actions(params)
+        "input.set_input_action":
+            return input_commands.set_input_action(params)
+        # node.* routes for scene editor node tools (5 tools)
+        "node.duplicate":
+            return scene_editor_inst.duplicate_node(params)
+        "node.move":
+            return scene_editor_inst.move_node(params)
+        "node.connect_signal":
+            return scene_editor_inst.connect_signal(params)
+        "node.disconnect_signal":
+            return scene_editor_inst.disconnect_signal(params)
+        "node.rename":
+            return scene_editor_inst.rename_node(params)
         _:
             return { "error": { "code": -32601, "message": "Method not found: %s" % method } }
 
